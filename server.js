3120863b0f9022321957a2f7e5736a7893371d00
gowthaman-developer <98395972+gowthaman-developer@users.noreply.github.com>
2022-03-13 10:49:41 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+// Require dependencies
+const logger = require("morgan");
+const express = require("express");
+
+// Require the needed functions
+const { sendResponse } = require("./app/helpers");
+const { fetchAuthorProfile } = require("./app/scotch");
+
+// Add the Scotch author profile route
+app.get("/scotch/:author", (req, res, next) => {
+  const author = req.params.author;
+  sendResponse(res)(fetchAuthorProfile(author));
+});
+
+// Create an Express application
+const app = express();
+
+// Configure the app port
+const port = process.env.PORT || 3000;
+app.set("port", port);
+
+// Load middlewares
+app.use(logger("dev"));
+
+// Start the server and listen on the preconfigured port
+app.listen(port, () => console.log(`App started on port ${port}.`));
